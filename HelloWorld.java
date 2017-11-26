0557d07595b097422948ce30e5c058cac81dbf57
Sanifrey <33393910+Sanifrey@users.noreply.github.com>
2017-11-26 21:15:47 +0500

    Фио

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,7 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("16130567"
+drawCenteredString("Нурмухаметов"
 		+ "16130569"
 		+ "16130029"
 		+ "16130163", d.width, d.height, g);
